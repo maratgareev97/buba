f9b886c99e9d7e4a729945998bd1de99074930be
marat <maratgareev97@gmail.com>
2023-11-24 15:45:45 +0300

    1

--- dataBase.py
+++ dataBase.py
@@ -1,22 +1,31 @@
 import pymysql
+from flask import Flask, render_template, request, redirect
 
-try:
-    connection = pymysql.connect(host='82.146.35.88',
-                                         user='school',
-                                         password='Q1w2e3r4',
-                                         db='school',
-                                         charset='cp1251',
-                                         cursorclass=pymysql.cursors.DictCursor)
-    print("С базой соединились")
-except:
-    print("Ошибка соединения")
+app = Flask(__name__)
 
 
-cur = connection.cursor() # создание курсора
-cur.execute("SELECT * FROM buba;") # это сам запрос
-result = cur.fetchall()  # перевод ответа запроса в виде строки
+@app.route('/')
+def zopros():
+    try:
+        connection = pymysql.connect(host='82.146.35.88',
+                                     user='school',
+                                     password='Q1w2e3r4',
+                                     db='school',
+                                     charset='cp1251',
+                                     cursorclass=pymysql.cursors.DictCursor)
+        print("С базой соединились")
+    except:
+        print("Ошибка соединения")
 
-cur.close() # закрытие курсова
-connection.close() # закрытие соединения
+    cur = connection.cursor()  # создание курсора
+    cur.execute("SELECT * FROM buba;")  # это сам запрос
+    result = cur.fetchall()  # перевод ответа запроса в виде строки
 
-print(result)
+    cur.close()  # закрытие курсова
+    connection.close()  # закрытие соединения
+
+    print(result)
+    return render_template("zapros.html", res=result)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
